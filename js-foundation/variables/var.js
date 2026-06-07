881c9ea38f797144a80bbb355d4bf34e9662d83f
Adesh_kumar <adeshkumar04444@gmail.com>
2026-06-07 21:17:28 +0530

    add comment

--- js-foundation/variables/var.js
+++ js-foundation/variables/var.js
@@ -1,3 +1,7 @@
+/*
+var: Oldest way to declare variables. Function-scoped, can be re-declared and re-assigned.
+*/
+
 var greeting = "Hello";
 var greeting = "Hi";    // Re-declaration is allowed
 var greeting = "Hey";   // Re-assignment is allowed
