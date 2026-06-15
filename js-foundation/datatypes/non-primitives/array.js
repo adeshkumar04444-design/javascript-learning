39a84097a4b6a7174ae6f6aac0d1953c324ed4c7
Adesh_kumar <adeshkumar04444@gmail.com>
2026-06-15 14:27:24 +0530

    added array datatype example

---
+++ js-foundation/datatypes/non-primitives/array.js
@@ -0,0 +1,6 @@
+// Array: A special type of object used to store ordered collections of data. Arrays can hold elements of any data type, including other arrays and objects. They are zero-indexed, meaning the first element is accessed with index 0.
+
+let colors = ["red", "green", "blue"];
+console.log(typeof colors); // Output: "object" (Arrays are objects in JS)
+console.log(colors[0]); // Output: "red"
+console.log(colors.length); // Output: 3    
