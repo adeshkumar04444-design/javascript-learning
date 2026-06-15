7408ad12c7759ececabdd0d40e97cc938b946168
Adesh_kumar <adeshkumar04444@gmail.com>
2026-06-15 13:44:00 +0530

    added number datatype example

---
+++ js-foundation/datatypes/primitives/number.js
@@ -0,0 +1,6 @@
+// Number: Represents both integers and floating-point numbers.
+
+let age = 30;
+let price = 99.99;
+console.log(typeof age); // Output: "number"
+console.log(typeof price); // Output: "number"
