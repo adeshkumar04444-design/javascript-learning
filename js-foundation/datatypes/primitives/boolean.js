03aa2010b1073e113c61eae09a9eaa3cccb8b5b0
Adesh_kumar <adeshkumar04444@gmail.com>
2026-06-15 13:49:51 +0530

    added boolean datatype example

---
+++ js-foundation/datatypes/primitives/boolean.js
@@ -0,0 +1,6 @@
+// Boolean: Represents true or false values.
+
+let isActive = true;
+let hasPermission = false;
+console.log(typeof isActive); // Output: "boolean"
+console.log(typeof hasPermission); // Output: "boolean"
