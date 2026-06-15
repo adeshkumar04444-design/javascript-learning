59379769844689bfa69373748179e03755a7ade1
Adesh_kumar <adeshkumar04444@gmail.com>
2026-06-15 14:35:37 +0530

    added function datatype example

---
+++ js-foundation/datatypes/non-primitives/function.js
@@ -0,0 +1,7 @@
+// Function: A block of code designed to perform a particular task. Functions are also objects in JS.
+
+function greet() {
+  console.log("Hello!");
+}
+console.log(typeof greet); // Output: "function" (though functions technically an objects)
+greet(); // Output: "Hello!"
