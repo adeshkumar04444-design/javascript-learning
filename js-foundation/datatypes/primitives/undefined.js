279be006c3712f2f18f0e961d7b8a0da6bb1aeb2
Adesh_kumar <adeshkumar04444@gmail.com>
2026-06-15 13:56:03 +0530

    added undefined datatype example

---
+++ js-foundation/datatypes/primitives/undefined.js
@@ -0,0 +1,5 @@
+// Undefined: A variable that has been declared but not yet assigned a value.
+
+let undefinedVar;
+console.log(undefinedVar); // Output: undefined
+console.log(typeof undefinedVar); // Output: "undefined"
