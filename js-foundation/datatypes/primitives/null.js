7c6a9dab566443a61c463fc86f919a021e1a3c45
Adesh_kumar <adeshkumar04444@gmail.com>
2026-06-15 14:03:10 +0530

    added null datatype example

---
+++ js-foundation/datatypes/primitives/null.js
@@ -0,0 +1,5 @@
+// Null: Represents the intentional absence of any object value.It's a primitive value.
+
+let emptyValue = null;
+console.log(emptyValue); // Output: null
+console.log(typeof emptyValue); // Output: "object" (this is a long-standing bug in JavaScript)
