4cc3cb39304f8f3ed48e953d817b7f7478097373
Adesh_kumar <adeshkumar04444@gmail.com>
2026-06-16 18:38:44 +0530

    added switch control flow example

---
+++ js-foundation/control-flow/switch.js
@@ -0,0 +1,13 @@
+let day = "Monday";
+switch (day) {
+  case "Monday":
+    console.log("Start of the work week.");
+    break;
+  case "Friday":
+    console.log("End of the work week!");
+    break;
+  default:
+    console.log("Just another day.");
+}
+
+// Output: Start of the work week.
