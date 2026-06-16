c5af7f5fbac9e4572a8f4b903f20767f56a08f95
Adesh_kumar <adeshkumar04444@gmail.com>
2026-06-16 10:14:26 +0530

    added assingnment operator examples

---
+++ js-foundation/operators/assignment-operators.js
@@ -0,0 +1,25 @@
+// Assignment Operators: =, +=, -=, *=, /=, %=, **=
+
+let x = 5;
+x += 3; // x = x + 3; // x is now 8
+console.log(x); // 8
+
+let y = 10;
+y -= 2; // y = y - 2; // y is now 8
+console.log(y); // 8
+
+let z = 4;
+z *= 2; // z = z * 2; // z is now 8
+console.log(z); // 8
+
+let w = 16;
+w /= 2; // w = w / 2; // w is now 8
+console.log(w); // 8
+
+let m = 17;
+m %= 3; // m = m % 3; // m is now 2
+console.log(m); // 2
+
+let n = 2;
+n **= 3; //n = n ** 3; // n is now 8
+console.log(n); // 8
