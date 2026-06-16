1a44a54e3117316b654333091751904775a78661
Adesh_kumar <adeshkumar04444@gmail.com>
2026-06-16 10:21:54 +0530

    added comparison operator examples

---
+++ js-foundation/operators/comparison-operators.js
@@ -0,0 +1,10 @@
+// Comparison Operators: ==(loose equality), === (strict equality), !=, !==, >, <, >=, <=
+
+console.log(5 == '5'); // true (loose equality, type coercion)
+console.log(5 === '5'); // false (strict equality, no type coercion)
+console.log(5 != '5'); // false (loose inequality)
+console.log(5 !== '5'); // true (strict inequality)
+console.log(5 > 3); // true (greater than)
+console.log(5 < 3); // false (less than)
+console.log(5 >= 3); // true (greater than or equal to)
+console.log(5 <= 3); // false (less than or equal to)
