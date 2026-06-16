b90563c27adb1fb5846872292a90261ff645041b
Adesh_kumar <adeshkumar04444@gmail.com>
2026-06-16 10:27:27 +0530

    added logical operator examples

---
+++ js-foundation/operators/logical-operators.js
@@ -0,0 +1,7 @@
+// Logical Operators: && (AND), || (OR), ! (NOT)
+
+let isAdult = true;
+let hasLicense = false;
+console.log(isAdult && hasLicense); // false (AND operator, both conditions must be true)
+console.log(isAdult || hasLicense); // true (OR operator, at least one condition must be true)
+console.log(!isAdult); // false (NOT operator, negates the boolean value)
