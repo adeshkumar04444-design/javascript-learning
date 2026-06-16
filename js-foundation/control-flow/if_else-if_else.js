ec4a50769811d0e3d624e0baf2544d2f09fa12fc
Adesh_kumar <adeshkumar04444@gmail.com>
2026-06-16 18:30:09 +0530

    added if_else-if_else control flow example

---
+++ js-foundation/control-flow/if_else-if_else.js
@@ -0,0 +1,12 @@
+let temperature = 25;
+if (temperature > 30) {
+    console.log("It's hot outside!");
+}
+else if (temperature > 20) {
+    console.log("It's pleasant.");
+}
+else {
+    console.log("It's cold.");
+}
+
+// Output: It's pleasant.
