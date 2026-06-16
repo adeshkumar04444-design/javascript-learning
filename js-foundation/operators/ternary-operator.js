b8d0180347d18144bf568bdaca2e96949e87209c
Adesh_kumar <adeshkumar04444@gmail.com>
2026-06-16 10:32:51 +0530

    added ternary operator example

---
+++ js-foundation/operators/ternary-operator.js
@@ -0,0 +1,9 @@
+// Ternary Operator: condition ? expressionIfTrue : expressionIfFalse
+
+let age = 18;
+let status = age >= 18 ? "Adult" : "Minor";
+console.log(status); // "Adult"
+
+age = 16;
+status = age >= 18 ? "Adult" : "Minor";
+console.log(status); // "Minor"
