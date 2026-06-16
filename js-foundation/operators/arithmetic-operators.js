54bbbb059992d821fe35c68ff317500f87413c3f
Adesh_kumar <adeshkumar04444@gmail.com>
2026-06-16 09:56:25 +0530

    added arithmetic operator examples

---
+++ js-foundation/operators/arithmetic-operators.js
@@ -0,0 +1,11 @@
+// Arithmetic Operators: +,-,*,/,%(modulus),**(exponentiation),++(increment),--(decrement)
+
+let a = 10, b = 3;
+console.log("Addition: ", a + b); // 13
+console.log("Subtraction: ", a - b); // 7
+console.log("Multiplication: ", a * b); // 30
+console.log("Division: ", a / b); // 3.333...
+console.log("Modulus: ", a % b); // 1
+console.log("Exponentiation: ", a ** b); // 1000    
+console.log("Increment: ", ++a); // 11
+console.log("Decrement: ", --b); // 2
